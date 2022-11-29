7d96ee1a0d659b14dcd4bfcb662655864230f7dd
Dave Goldsmith <davegoldsmith@hotmail.com>
2022-11-29 12:03:30 +0000

    First Attempt at exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,65 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+  return nums.map(num => num * num);
+
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+  let camelled = '';
+  words.forEach((word, index) => {
+    if (index === 0) {
+      camelled += word;
+    } else {
+      camelled += word.charAt(0).toUpperCase() + word.substring(1, word.length);
+    }
+  });
+  return camelled;
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  let counter = 0;
+  people.forEach(person => {
+     if (person.subjects.length > 0) {
+       counter+=person.subjects.length;
+     }
+  });
+  return counter;
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  let result = false;
+  menu.forEach(menuItem => {
+    // use 'for of loop' to allow break statement once
+    // we determine the ingredient is somewhere in the menu
+    // then we want to stop
+    for (const menuIngredient of menuItem.ingredients) {
+      if (menuIngredient === ingredient) {
+        result = true;
+        break;
+      }
+    }
+  });
+
+  return result;
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+  let result = [];
+  arr1.forEach(element => {    
+    if (arr2.includes(element) && result.includes(element) === false) { 
+      result.push(element);
+    }
+  });
+  return result.sort();
 }
